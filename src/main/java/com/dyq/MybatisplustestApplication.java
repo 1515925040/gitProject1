9b6f4c2e348e0c533a15e3080a1bd4e139a8da5f
dyq <992504740@qq.com>
2019-08-19 21:53:49 +0800

    git主干提交

---
+++ src/main/java/com/dyq/MybatisplustestApplication.java
@@ -0,0 +1,16 @@
+package com.dyq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplustestApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("mybatis测试");
+        SpringApplication.run(MybatisplustestApplication.class, args);
+
+    }
+
+}
